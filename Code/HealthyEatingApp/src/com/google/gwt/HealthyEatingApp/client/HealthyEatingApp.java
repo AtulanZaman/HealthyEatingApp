4b3ac5ac717ee671bc28055700c8b4dacd2e3460
Rashna Razdan <rashnarazdan@gmail.com>
2013-06-12 18:31:00 -0400

    no message

--- Code/HealthyEatingApp/src/com/google/gwt/HealthyEatingApp/client/HealthyEatingApp.java
+++ Code/HealthyEatingApp/src/com/google/gwt/HealthyEatingApp/client/HealthyEatingApp.java
@@ -1,152 +1,122 @@
 package com.google.gwt.HealthyEatingApp.client;
 
-import com.google.gwt.HealthyEatingApp.shared.FieldVerifier;
+import java.util.ArrayList;
+
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
+
+
+
+
 public class HealthyEatingApp implements EntryPoint {
+
+
+private VerticalPanel mainPanel = new VerticalPanel();
+private FlexTable stocksFlexTable = new FlexTable();
+private HorizontalPanel addPanel = new HorizontalPanel();
+private TextBox newSymbolTextBox = new TextBox();
+private Button addStockButton = new Button("Add");
+private Label lastUpdatedLabel = new Label();
+private ArrayList<String> stocks = new ArrayList<String>();
+
+  
+  
+  /**
+   * Entry point method.
+   */
+  public void onModuleLoad() {
+	  // Create table for stock data.
+	    stocksFlexTable.setText(0, 0, "Type of Meal");
+	    stocksFlexTable.setText(0, 1, "Food Item         ");
+	 
+	    // Assemble Add Stock panel.
+	    addPanel.add(newSymbolTextBox);
+	    addPanel.add(addStockButton);
+
+	    // Assemble Main panel.
+	    mainPanel.add(stocksFlexTable);
+	    mainPanel.add(addPanel);
+	    mainPanel.add(lastUpdatedLabel);
+
+	    // Associate the Main panel with the HTML host page.
+	    RootPanel.get("userEntry").add(mainPanel);
+	    // Move cursor focus to the input box.
+	    newSymbolTextBox.setFocus(true);
+	    
+	    // Listen for mouse events on the Add button.
+	    addStockButton.addClickHandler(new ClickHandler() {
+	      public void onClick(ClickEvent event) {
+	        addStock();
+	      }
+	    });
+	    
+	    // Listen for keyboard events in the input box.
+	    newSymbolTextBox.addKeyPressHandler(new KeyPressHandler() {
+	      public void onKeyPress(KeyPressEvent event) {
+	        if (event.getCharCode() == KeyCodes.KEY_ENTER) {
+	          addStock();
+	        }
+	      }
+	    });
+	    
+
+
+	  }
+
+	 
+	
 	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
+	   * Add stock to FlexTable. Executed when the user clicks the addStockButton or
+	   * presses enter in the newSymbolTextBox.
+	   */
+	  private void addStock() {
+		  final String symbol = newSymbolTextBox.getText().toUpperCase().trim();
+		    newSymbolTextBox.setFocus(true);
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT
-			.create(GreetingService.class);
+		    // Stock code must be between 1 and 10 chars that are numbers, letters, or dots.
+		    if (!symbol.matches("^[0-9A-Z\\.]{1,10}$")) {
+		      Window.alert("'" + symbol + "' is not a valid symbol.");
+		      newSymbolTextBox.selectAll();
+		      return;
+		    }
 
-	/**
-	 * This is the entry point method.
-	 */
-	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
+		    newSymbolTextBox.setText("");
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
+		    // Don't add the stock if it's already in the table.
+		    if (stocks.contains(symbol))
+		      return;
+		    
+		    // Add the stock to the table.
+		    int row = stocksFlexTable.getRowCount();
+		    stocks.add(symbol);
+		    stocksFlexTable.setText(row, 0, symbol);
 
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
+		    // Add a button to remove this stock from the table.
+		    Button removeStockButton = new Button("x");
+		    removeStockButton.addClickHandler(new ClickHandler() {
+		      public void onClick(ClickEvent event) {
+		        int removedIndex = stocks.indexOf(symbol);
+		        stocks.remove(removedIndex);        
+		        stocksFlexTable.removeRow(removedIndex + 1);
+		      }
+		    });
+		    stocksFlexTable.setWidget(row, 3, removeStockButton);
+		    
+		    
+	  }
 
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
+  }
 
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer,
-						new AsyncCallback<String>() {
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-
-							public void onSuccess(String result) {
-								dialogBox.setText("Remote Procedure Call");
-								serverResponseLabel
-										.removeStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(result);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-						});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
-	}
-}
