c6811519cfee372a1a9354074d79ebaa23a8ce4d
Rashna Razdan <rashnarazdan@gmail.com>
2013-06-12 22:17:38 -0400

    -map out basic UI -preliminary input layout for user food log entry

--- Code/HealthyEatingApp/src/com/google/gwt/HealthyEatingApp/client/HealthyEatingApp.java
+++ Code/HealthyEatingApp/src/com/google/gwt/HealthyEatingApp/client/HealthyEatingApp.java
@@ -7,9 +7,12 @@ import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TabPanel;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Label;
 
@@ -18,15 +21,27 @@ import com.google.gwt.user.client.ui.Label;
  */
 public class HealthyEatingApp implements EntryPoint, ValueChangeHandler {
 
- VerticalPanel panel = new VerticalPanel();
-    Label label=new Label();
+ VerticalPanel verticalPanel = new VerticalPanel();
+
+ Label labelMealType=new Label();
+ Label labelFoodName=new Label();
+ Label labelLink=new Label();
+ TextBox meal = new TextBox();
+ TextBox foodName = new TextBox();
+ 
+
+    
     public void onModuleLoad() {
-        Hyperlink link1 = new Hyperlink("Food Logs", "log");
-        Hyperlink link2 = new Hyperlink("Graphical Information", "graph");
-        panel.add(link1);
-        panel.add(link2);
-        panel.add(label);
-        RootPanel.get().add(panel);
+        Hyperlink link1 = new Hyperlink("See Past Food Logs", "log");
+        Hyperlink link2 = new Hyperlink("Enter New Food Log", "newEntry");
+        Hyperlink link3 = new Hyperlink("Graphical Information", "graph");
+        Hyperlink link4 = new Hyperlink("Leaderboard", "leaderboard");
+        verticalPanel.add(link1);
+        verticalPanel.add(link2);
+        verticalPanel.add(link3);
+        verticalPanel.add(link4);
+        verticalPanel.add(labelLink);
+
         History.addValueChangeHandler(this);
         //when there is no token, the "home" token is set else changePage() is called.
         //this is useful if a user has bookmarked a site other than the homepage.
@@ -35,6 +50,9 @@ public class HealthyEatingApp implements EntryPoint, ValueChangeHandler {
         } else {
             changePage(History.getToken());
         }
+        
+        RootPanel.get().add(verticalPanel);
+
     }
 
 
@@ -43,11 +61,49 @@ public void onValueChange(ValueChangeEvent event) {
 }
 public void changePage(String token) {
     if(History.getToken().equals("log")) {
-        label.setText("Here are this months food logs");
+    	labelLink.setText("Here are this months food logs");
+    	
+    	//hide New Entry display
+  	  	labelLink.setText("Checkout the leaderboard based on other Facebook friends using this application");
+  	  	verticalPanel.remove(meal);
+  	  	verticalPanel.remove(foodName);
+  	  	verticalPanel.remove(labelFoodName);
+  	  	verticalPanel.remove(labelMealType);
+
     } else if (History.getToken().equals("graph")) {
-        label.setText("Here is a graph showing your stats");
-    } else {
-        label.setText("This is the profile page that contains snippets of all relevant information available via this application");
+    	labelLink.setText("Here is a graph showing your stats");
+    	
+    	//hide New Entry display
+  	  	labelLink.setText("Checkout the leaderboard based on other Facebook friends using this application");
+  	  	verticalPanel.remove(meal);
+  	  	verticalPanel.remove(foodName);
+  	  	verticalPanel.remove(labelFoodName);
+  	  	verticalPanel.remove(labelMealType);
+
+    }
+      else if (History.getToken().equals("newEntry")) {
+    	  //New Entry display
+    	  labelLink.setText("Add new entry to your food log");	
+          verticalPanel.add(labelMealType);
+    	  labelMealType.setText("Meal:");
+    	  verticalPanel.add(meal);
+          verticalPanel.add(labelFoodName);
+    	  labelFoodName.setText("Food item:");
+          verticalPanel.add(foodName);
+
+      }
+      else if (History.getToken().equals("leaderboard")) {
+    	  
+    	  //hide New Entry display
+    	  labelLink.setText("Checkout the leaderboard based on other Facebook friends using this application");
+    	  verticalPanel.remove(meal);
+    	  verticalPanel.remove(foodName);
+    	  verticalPanel.remove(labelFoodName);
+    	  verticalPanel.remove(labelMealType);
+
+    }
+      else {
+    	  labelLink.setText("This is the profile page that contains snippets of all relevant information available via this application");
     }
 }
 }
