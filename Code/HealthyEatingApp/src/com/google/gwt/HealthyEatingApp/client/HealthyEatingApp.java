fbfcedb98292f17cbfe5e1eed93d13dc840a579c
Rashna Razdan <rashnarazdan@gmail.com>
2013-06-12 20:13:47 -0400

    add skeleton outline

--- Code/HealthyEatingApp/src/com/google/gwt/HealthyEatingApp/client/HealthyEatingApp.java
+++ Code/HealthyEatingApp/src/com/google/gwt/HealthyEatingApp/client/HealthyEatingApp.java
@@ -7,52 +7,48 @@ import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TabPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Label;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class HealthyEatingApp implements EntryPoint {
+public class HealthyEatingApp implements EntryPoint, ValueChangeHandler {
 
-  TabPanel tabPanel;
-  /**
-   * This is the entry point method.
-   */
-  public void onModuleLoad() {
-    tabPanel = new TabPanel();
-
-    tabPanel.add(new HTML("<h1>Page 0 Content: </h1>"), " Page 0 ");
-    tabPanel.add(new HTML("<h1>Page 1 Content: </h1>"), " Page 1 ");
-    tabPanel.add(new HTML("<h1>Page 2 Content: </h1>"), " Page 2 ");
-
-    tabPanel.addSelectionHandler(new SelectionHandler<Integer>(){
-      public void onSelection(SelectionEvent<Integer> event) {
-        History.newItem("page" + event.getSelectedItem());
-      }});
-
-    History.addValueChangeHandler(new ValueChangeHandler<String>() {
-      public void onValueChange(ValueChangeEvent<String> event) {
-        String historyToken = event.getValue();
-
-        // Parse the history token
-        try {
-          if (historyToken.substring(0, 4).equals("page")) {
-            String tabIndexToken = historyToken.substring(4, 5);
-            int tabIndex = Integer.parseInt(tabIndexToken);
-            // Select the specified tab panel
-            tabPanel.selectTab(tabIndex);
-          } else {
-            tabPanel.selectTab(0);
-          }
-
-        } catch (IndexOutOfBoundsException e) {
-          tabPanel.selectTab(0);
+ VerticalPanel panel = new VerticalPanel();
+    Label label=new Label();
+    public void onModuleLoad() {
+        Hyperlink link1 = new Hyperlink("Food Logs", "log");
+        Hyperlink link2 = new Hyperlink("Graphical Information", "graph");
+        panel.add(link1);
+        panel.add(link2);
+        panel.add(label);
+        RootPanel.get().add(panel);
+        History.addValueChangeHandler(this);
+        //when there is no token, the "home" token is set else changePage() is called.
+        //this is useful if a user has bookmarked a site other than the homepage.
+        if(History.getToken().isEmpty()){
+            History.newItem("home");
+        } else {
+            changePage(History.getToken());
         }
-      }
-    });
+    }
 
-    tabPanel.selectTab(0);
-    RootPanel.get().add(tabPanel);
-  }
+
+public void onValueChange(ValueChangeEvent event) {
+    changePage(History.getToken());
 }
+public void changePage(String token) {
+    if(History.getToken().equals("log")) {
+        label.setText("Here are this months food logs");
+    } else if (History.getToken().equals("graph")) {
+        label.setText("Here is a graph showing your stats");
+    } else {
+        label.setText("This is the profile page that contains snippets of all relevant information available via this application");
+    }
+}
+}
+    
