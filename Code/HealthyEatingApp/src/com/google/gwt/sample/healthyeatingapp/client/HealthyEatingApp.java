3b1bfa9b206ff953695d65371ab0a9edc3b51b08
Rashna Razdan <rashnarazdan@gmail.com>
2013-07-05 11:47:44 -0400

    no message

--- Code/HealthyEatingApp/src/com/google/gwt/sample/healthyeatingapp/client/HealthyEatingApp.java
+++ Code/HealthyEatingApp/src/com/google/gwt/sample/healthyeatingapp/client/HealthyEatingApp.java
@@ -1,6 +1,10 @@
 package com.google.gwt.sample.healthyeatingapp.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.gwt.user.client.ui.TabLayoutPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -12,8 +16,11 @@ public class HealthyEatingApp implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		
-		
-
+		 
+		TabLayoutPanel homepage = new TabLayoutPanel(2.5, Unit.EM);
+		homepage.add(new HTML(""), "Graph");
+		homepage.add(new HTML(""), "Log");
+		homepage.add(new HTML(""), "Social");
+		RootLayoutPanel.get().add(homepage);
 	}
 }
