9937c08825854ecc3a4c650f1cfa0bfd024c6e7b
AtulanZaman <atulan.zaman@gmail.com>
2013-07-07 12:34:47 -0400

    - Commits Working line graph for static data - Need to make it work for dynamic queries - Adds .gitignore

--- Code/HealthyEatingApp/src/com/google/gwt/sample/healthyeatingapp/client/HealthyEatingApp.java
+++ Code/HealthyEatingApp/src/com/google/gwt/sample/healthyeatingapp/client/HealthyEatingApp.java
@@ -5,22 +5,34 @@ import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.TabLayoutPanel;
+import com.google.gwt.visualization.client.VisualizationUtils;
+import com.google.gwt.visualization.client.visualizations.corechart.LineChart;
+
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class HealthyEatingApp implements EntryPoint {
-
 	
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		 
-		TabLayoutPanel homepage = new TabLayoutPanel(2.5, Unit.EM);
-		homepage.add(new HTML(""), "Graph");
-		homepage.add(new HTML(""), "Log");
-		homepage.add(new HTML(""), "Social");
-		RootLayoutPanel.get().add(homepage);
+		Runnable onLoadCallBack = new Runnable(){
+		public void run(){
+				TabLayoutPanel homepage = new TabLayoutPanel(2.5, Unit.EM);
+				
+				homepage.add(new Graph().returnGraph(), "Graph");
+				homepage.add(new HTML(""), "Log");
+				homepage.add(new HTML(""), "Social");
+				RootLayoutPanel.get().add(homepage);
+				
+				
+			}
+		}; 
+     //Create a callback to be called when the visualization API
+     //has been loaded.
+		VisualizationUtils.loadVisualizationApi(onLoadCallBack, LineChart.PACKAGE);
+		
 	}
 }
--- Code/HealthyEatingApp/src/com/google/gwt/sample/healthyeatingapp/client/Homepage.java
+++ Code/HealthyEatingApp/src/com/google/gwt/sample/healthyeatingapp/client/Homepage.java
@@ -1,9 +1,6 @@
 package com.google.gwt.sample.healthyeatingapp.client;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Widget;
 
 public class Homepage extends Composite  {
 
